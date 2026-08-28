c7521c3e7bb4b72da40937606b47b22faffd100e
Phong Bui <phongbuiphotolx@gmail.com>
2026-08-28 09:41:13 +0700

    Fix vercel configuration and serverless function routing error

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
 const app = require('../server');
 
-module.exports = app;
+module.exports = async function handler(req, res) {
+  try {
+    return app(req, res);
+  } catch (err) {
+    console.error('[Vercel Serverless Function Crash Prevention]:', err);
+    if (!res.headersSent) {
+      return res.status(500).json({
+        success: false,
+        error: err.message || 'Internal Serverless Function Error'
+      });
+    }
+  }
+};
